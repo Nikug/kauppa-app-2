0c3b2356d28ae191b7c4f3deb4a95ea05a8464aa
nikugronberg <niku.gronberg@student.lut.fi>
2021-07-21 18:35:23 +0300

    add linting, prettier, tailwind

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "eslint:recommended",
+    "react-app",
+    "react-app/jest",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
